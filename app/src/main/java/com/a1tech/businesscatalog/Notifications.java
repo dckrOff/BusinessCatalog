84e6e988ae176cd5887f928fc058f53bce11d0f2
dckrOff <azamatotaboev7621@gmail.com>
2022-07-07 18:44:45 +0500

    added cloud messaging

---
+++ app/src/main/java/com/a1tech/businesscatalog/Notifications.java
@@ -0,0 +1,41 @@
+package com.a1tech.businesscatalog;
+
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+public class Notifications extends FirebaseMessagingService {
+
+    public Notifications() {
+        super();
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
+
+    @Override
+    public void onMessageSent(@NonNull String msgId) {
+        super.onMessageSent(msgId);
+    }
+
+    @Override
+    public void onSendError(@NonNull String msgId, @NonNull Exception exception) {
+        super.onSendError(msgId, exception);
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+    }
+}
